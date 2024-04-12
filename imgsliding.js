c4dcc0824d6c8f14b93b9bca666c9240217de408
kikisuuu <162807972+kikisuuu@users.noreply.github.com>
2024-04-13 03:45:55 +0800

    Add files via upload

--- imgsliding.js
+++ imgsliding.js
@@ -1,43 +1,19 @@
-// Get the DOM elements for the image carousel
-const wrapper = document.querySelector(".wrapper"),
-  carousel = document.querySelector(".carousel"),
-  images = document.querySelectorAll("img"),
-  buttons = document.querySelectorAll(".button");
+let slideIndex = 0;
+showSlides();
 
-let imageIndex = 1,
-  intervalId;
-
-// Define function to start automatic image slider
-const autoSlide = () => {
-  // Start the slideshow by calling slideImage() every 2 seconds
-  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
-};
-// Call autoSlide function on page load
-autoSlide();
-
-// A function that updates the carousel display to show the specified image
-const slideImage = () => {
-  // Calculate the updated image index
-  imageIndex = imageIndex === images.length ? 0 : imageIndex < 0 ? images.length - 1 : imageIndex;
-  // Update the carousel display to show the specified image
-  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
-};
-
-// A function that updates the carousel display to show the next or previous image
-const updateClick = (e) => {
-  // Stop the automatic slideshow
-  clearInterval(intervalId);
-  // Calculate the updated image index based on the button clicked
-  imageIndex += e.target.id === "next" ? 1 : -1;
-  slideImage(imageIndex);
-  // Restart the automatic slideshow
-  autoSlide();
-};
-
-// Add event listeners to the navigation buttons
-buttons.forEach((button) => button.addEventListener("click", updateClick));
-
-// Add mouseover event listener to wrapper element to stop auto sliding
-wrapper.addEventListener("mouseover", () => clearInterval(intervalId));
-// Add mouseleave event listener to wrapper element to start auto sliding again
-wrapper.addEventListener("mouseleave", autoSlide);
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
